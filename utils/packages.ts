2bb0a8b0faca7fc5ecbd2cb3636a3b26b47ea296
Nnaemeka Bethrand <bethrand2019@gmail.com>
2024-12-06 19:00:08 +0100

    [FEAT] : Did alot [Added Form and integrated it]

--- utils/packages.ts
+++ utils/packages.ts
@@ -1,68 +1,103 @@
-export     const examplePackages = [
-    {
-        packageTitle: 'Festive Bliss Package',
-        price: '₦40,000',
-        items: [
-            {
-                value: 'provision',
-                itemContent: [
-                    'Checkers Custard',
-                    'Kellogs Cornflakes',
-                    'Golden Penny Spread',
-                    'Dano cool cow',
-                    'Milo',
-                    'Golden Penny Sugar',
-                    'Chivita 1L',
-                    'McVities Digestive',
-                    'Plantain Chips',
-                ],
-            },
-            {
-                value: 'food',
-                itemContent: [
-                    '1 Ltr vegetable oil',
-                    'Knorr Seasoning Cubes',
-                    'Mr. Chef Salt (x2)',
-                    '5KG rice',
-                    '1 Golden Penny Spaghetti ',
-                    '1 Twist ',
-                    'Gino Pepper & Onion Sachet Tomatoes (x5)',
-                    'Whippy Mayonnaise',
-                ],
-            },
+export const examplePackages = [
+  {
+    packageTitle: "Festive Bliss Package",
+    price: "₦40,000",
+    items: [
+      {
+        value: "provision",
+        itemContent: [
+          "Checkers Custard",
+          "Kellogs Cornflakes",
+          "Golden Penny Spread",
+          "Dano cool cow",
+          "Milo",
+          "Golden Penny Sugar",
+          "Chivita 1L",
+          "McVities Digestive",
+          "Plantain Chips",
         ],
-    },
-    {
-        packageTitle: 'Deluxe Package',
-        price: '₦60,000',
-        items: [
-            {
-                value: 'provision',
-                itemContent: [
-                    '4th Street Wine',
-                    'Bottle of Groundnut ',
-                    'Checkers Custard',
-                    'Dano Cool Cow',
-                    'McVities Shortbread',
-                    'Reel Fruit Caramel coconut Chips',
-                    'Plantain Chips',
-                    'Golden Penny Sugar ',
-                    'Bama Mayonnaise',
-                ],
-            },
-            {
-                value: 'food',
-                itemContent: [
-                    '1 Ltr vegetable oil',
-                    'Knorr Seasoning Cubes',
-                    'Mr. Chef Salt (x2)',
-                    '5KG rice',
-                    '1 Golden Penny Spaghetti ',
-                    '1 Twist ',
-                    'Gino Pepper & Onion Sachet Tomatoes (x5)',
-                    'Whippy Mayonnaise',
-                ],
-            },
+      },
+      {
+        value: "food",
+        itemContent: [
+          "1 Ltr vegetable oil",
+          "Knorr Seasoning Cubes",
+          "Mr. Chef Salt (x2)",
+          "5KG rice",
+          "1 Golden Penny Spaghetti ",
+          "1 Twist ",
+          "Gino Pepper & Onion Sachet Tomatoes (x5)",
+          "Whippy Mayonnaise",
         ],
-    },
+      },
+    ],
+  },
+  {
+    packageTitle: "Deluxe Package",
+    price: "₦60,000",
+    items: [
+      {
+        value: "provision",
+        itemContent: [
+          "4th Street Wine",
+          "Bottle of Groundnut ",
+          "Checkers Custard",
+          "Dano Cool Cow",
+          "McVities Shortbread",
+          "Reel Fruit Caramel coconut Chips",
+          "Plantain Chips",
+          "Golden Penny Sugar ",
+          "Bama Mayonnaise",
+        ],
+      },
+      {
+        value: "food",
+        itemContent: [
+          "1 Ltr vegetable oil",
+          "Knorr Seasoning Cubes",
+          "Mr. Chef Salt (x2)",
+          "5KG rice",
+          "1 Golden Penny Spaghetti ",
+          "1 Twist ",
+          "Gino Pepper & Onion Sachet Tomatoes (x5)",
+          "Whippy Mayonnaise",
+        ],
+      },
+    ],
+  },
+];
+
+export const packageOffers: {
+  packageOfferName: string;
+  packageOfferValue: string;
+}[] = [
+  { packageOfferName: "Provisions", packageOfferValue: "provision" },
+  { packageOfferName: "Food gourmet", packageOfferValue: "food_gourmet" },
+];
+
+export const christmasPackages: {
+  packageName: string;
+  packagePrice: string;
+  packageValue: string;
+}[] = [
+  {
+    packageName: "Joyful Starter Package",
+    packageValue: "joyful",
+    packagePrice: "28",
+  },
+  {
+    packageName: "Festive Bliss Package",
+    packageValue: "festive",
+    packagePrice: "40",
+  },
+  {
+    packageName: "Deluxe Package",
+    packageValue: "deluxe",
+    packagePrice: "60",
+  },
+  {
+    packageName: "Grand Celebration Package",
+    packageValue: "grand",
+    packagePrice: "100",
+  },
 ];
